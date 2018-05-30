ef7510a285f07c92087a97d4eabfb3d87a11e984
Andre <25297095+xvzero@users.noreply.github.com>
2018-05-30 15:32:12 -0700

    Add game over overlay after player death

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -3,10 +3,10 @@
 public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
-
+    public float restartDelay = 5f;
 
     Animator anim;
-
+    float restartTimer;
 
     void Awake()
     {
@@ -19,6 +19,12 @@ public class GameOverManager : MonoBehaviour
         if (playerHealth.currentHealth <= 0)
         {
             anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
         }
     }
 }
